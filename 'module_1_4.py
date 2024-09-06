3cfdc1c5b8bc9ac8c0df6c7338ad041aed0352b2
YES2474 <romakazeko985@gmail.com>
2024-09-06 12:18:18 +0300

    Add files via upload

---
+++ 'module_1_4.py
@@ -0,0 +1,7 @@
+my_string="Готика игра года"
+print(len(my_string))
+print("готика игра года".upper())
+print("готика игра года".lower())
+print("готика игра года".replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
