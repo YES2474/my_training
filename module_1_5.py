73bf625c191be3fbab7b24594c9f889581bf1198
YES2474 <romakazeko985@gmail.com>
2024-09-09 13:49:32 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+#immutable_var = 4, 6, 8, 'g', 20, True, False, 'apple'
+#print(immutable_var)
+#immutable_var[2]=200
+#"кортежи" неизменяемые списки
+
+mutable_list = ['200', "300", "250", "600"]
+mutable_list[3]='1000'
+print(mutable_list)
+#Этот спмсок можно менять так как он находится в квадратных скобках
