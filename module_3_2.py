5d8bbf72c6f95733cfe7bc6f34c322344c5a8ea3
YES2474 <romakazeko985@gmail.com>
2024-09-24 20:39:50 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,15 +1,30 @@
-def send_email(message, recipient,sender = "university.help@gmail.com"):
-    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif sender != "university.help@gmail.com":
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    if not recipient.endswith(".com") and not recipient.endswith(".ru") and not recipient.endswith(".net"):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+        return
 
-send_email('messagje','university.help@gmail.com','@.com')
-send_email('messagje','university.help@gmail.com')
-send_email('messagje','universi@.com')
-send_email('messagje','universi@.co')
-send_email('messagje','universi.com')
+    if not sender.endswith(".com") and not sender.endswith(".ru") and not sender.endswith(".net"):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+        return
+
+    if '@' not in recipient:
+        print("Адрес получателя некорректен, '@' отсутствует.")
+        return
+
+    if '@' not in sender:
+        print("Адрес отправителя некорректен, '@' отсутствует.")
+        return
+
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+    if sender == "university.help@gmail.com":
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
