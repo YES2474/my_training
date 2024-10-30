5a083fb37347fe7df9dc99ba0c1f213da4df4627
YES2474 <romakazeko985@gmail.com>
2024-10-30 15:17:12 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,21 +1,21 @@
+def custom_write(file_name, strings):
+    file = open(file_name, 'w+', encoding='utf-8')
+    strings_positions={}
+    for counter in range(0, len(strings)):
+        file.read()
+        bait_stroki = file.tell()
+        file.write(f'{strings[counter]}\n')
+        strings_positions[ (counter+1, bait_stroki)] = strings[counter]
+    file.close()
+    return strings_positions
+
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
-
-def custom_write(file_name, strings):
-    n = 0
-    elem = {}
-    for i in info:
-        file = open(file_name, 'a', encoding='utf-8')  # файл в режиме записи
-        tell = (file.tell())
-        n += 1
-        file.write(f'{i}\n')
-        file.close()
-        elem.update({(n, tell):i})
-    return elem
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
